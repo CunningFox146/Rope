b3c4af0451a13a5b8d411211f83d43aeec3d2838
Makar Dzezhamesau <dezhemesov.work@gmail.com>
2024-03-07 17:19:17 +0100

    More assets

---
+++ Assets/Scripts/Services/Traps/InfiniteRotation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Rope.Services.Traps
+{
+    public class InfiniteRotation : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+        
+        private void Update()
+        {
+            transform.Rotate(Vector3.forward * (_speed * Time.deltaTime));
+        }
+    }
+}
